7bb2644f4cbb363c0e7e5825056caf60a6cba1fa
Jahnavi Kandepu <jahnavi.kandepu@sasi.ac.in>
2026-08-23 20:09:12 +0530

    Increase API request timeout

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,7 +14,7 @@ const apiClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 15000,
+  timeout: 60000,
 });
 
 // Interceptor for future JWT authentication token injection
